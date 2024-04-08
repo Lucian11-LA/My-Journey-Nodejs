d7383ced5e44c346f889cc0f6c2decc06b8199a3
Lucian Alfred <lucianalfred60@gmail.com>
2024-04-08 13:00:09 +0100

    Configurando servidor node com http

--- Node Server/server.js
+++ Node Server/server.js
@@ -1,1 +1,18 @@
-server.js
+
+const http = require('http');
+
+const server= http.createServer((request,response)=>{
+	console.log('servidor acessado!')
+
+	const headers = request.headers;
+	const method = request.method;
+	const url = request.url;
+
+	console.log("Headers: ");
+	console.log(headers);
+	console.log("Method: "+method);
+	console.log("URL: "+url);
+	
+});
+
+server.listen(3000);
